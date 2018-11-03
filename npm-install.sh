035b6926ab3e2a11c97a012046a54eb6cf159291
jianan <1072772483@qq.com>
2018-11-03 22:37:57 +0800

    this is a quick commit

--- npm-install.sh
+++ npm-install.sh
@@ -1,5 +1,5 @@
-cd ./game-server && npm install -d
+cd ./game-server && cnpm install -d
 echo '============   game-server npm installed ============'
 cd ..
-cd ./web-server && npm install -d
+cd ./web-server && cnpm install -d
 echo '============   web-server npm installed ============'
